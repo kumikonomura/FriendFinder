a8f1fb55523c40bef52e1d35d4836db3ef3873cc
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-05-14 17:54:04 -0700

    go to survey button on home page works

---
+++ app/public/assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Click 'go to survey' button and take me to survey pag
+
+// Click submit and show me my friend match
+
+// Convert each user's results into an array of numbers
+
+// Compare difference between current user's scores against the other users
+// questions by questions.
+
+// Add up the differences to calculate the totalDifference
+
+// Use absolute value of the differences. No negative solutions
+
+// closest match will be the user with the least amount of difference
+
+// Once you've found the current user's compatible friend, display the
+// result as a modal pop-up
+
+// The modal should display both the name and picture of the closest match
+
